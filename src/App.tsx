d514ada77e50c2906f1549b74213113a0e42b2fa
Joma-kun <jomamiwa@gmail.com>
2024-04-24 20:31:02 +0900

    success selialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import createEngine, { DiagramModel, DefaultNodeModel, DefaultLinkModel } from '@projectstorm/react-diagrams';
-import * as React from 'react';
 import { CanvasWidget } from '@projectstorm/react-canvas-core';
+import { DiagramCanvasWidget } from "./DiagramCanvasWidget";
+import { Button } from "@mui/material";
 
-import { DemoCanvasWidget} from "./DemoCanvasWidget"
-
-function App() {
+export default () => {
     const engine = createEngine();
     const model = new DiagramModel();
 
@@ -12,31 +11,55 @@ function App() {
         name: "ノード1",
         color: "rgb(0, 199, 255)"
     });
-    node1.setPosition(300, 200);
+    node1.setPosition(100, 200);
     node1.addOutPort("1-1");
-    node1.addInPort("1-1");
     node1.addOutPort("1-2");
 
     const node2 = new DefaultNodeModel({
         name: "ノード2",
         color: "rgb(199, 0, 255)"
     });
-    node2.setPosition(600, 200);
+    node2.setPosition(300, 200);
     node2.addInPort("2-1");
 
+    const node3 = new DefaultNodeModel({
+        name: "ノード3",
+        color: "rgb(255, 199, 0)"
+    });
+    node3.setPosition(300, 300);
+    node3.addInPort("3-1");
 
     const link1 = new DefaultLinkModel();
+    link1.addLabel("temp");
     link1.setSourcePort(node1.getPort("1-1")!);
     link1.setTargetPort(node2.getPort("2-1")!);
 
-    model.addAll(node1, node2, link1);
+    model.addAll(node1, node2, node3, link1);
     engine.setModel(model);
 
-    return (
-        <DemoCanvasWidget>
-            <CanvasWidget engine={engine} />
-        </DemoCanvasWidget>
-    )
-}
 
-export default App
+    var jsonString = JSON.stringify(model.serialize());
+    var jsonObject = JSON.parse(jsonString);
+
+    // リンクのsourceとtargetのIDを抜き出す
+    var linksData = jsonObject.layers
+        .filter((layer: any) => layer.type === 'diagram-links')
+        .map((layer: any) => Object.values(layer.models))
+        .flat()
+        .map((link: any) => ({ source: link.source, target: link.target }));
+
+    // ノードのIDを抜き出す
+    var nodesData = jsonObject.layers
+        .filter((layer: any) => layer.type === 'diagram-nodes')
+        .map((layer: any) => Object.keys(layer.models));
+
+
+    return (
+        <DiagramCanvasWidget>
+            <div>
+                <Button onClick={() => console.log(linksData, nodesData)}>aaa</Button>
+            </div>
+            <CanvasWidget engine={engine} />
+        </DiagramCanvasWidget>
+    );
+};
